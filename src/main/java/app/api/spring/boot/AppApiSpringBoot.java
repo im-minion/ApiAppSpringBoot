25ea4fc406f5c4754cd9e200c1258e2047327f81
im-minion <vaibhavminiya@gmail.com>
2018-08-12 19:49:09 +0530

    initial commit

---
+++ src/main/java/app/api/spring/boot/AppApiSpringBoot.java
@@ -0,0 +1,12 @@
+package app.api.spring.boot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiSpringBoot {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(AppApiSpringBoot.class, args);
+	}
+
+}
---
+++ src/main/java/app/api/spring/boot/topic/TopicService.java
@@ -0,0 +1,29 @@
+package app.api.spring.boot.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	private List<Topic> topics = new ArrayList<>( Arrays.asList(
+			new Topic("a","b","c"),
+			new Topic("x","y","z"),
+			new Topic("p","q","r")
+			));
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	
+}
